de2d99b3382ee5679dc71a79df1d881acc689697
itsubaki <itsubaki@users.noreply.github.com>
2018-06-25 14:23:43 +0900

    Rename broker impl files

--- launcher/interstellar/main.go
+++ launcher/interstellar/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"log"
-
 	"github.com/itsubaki/interstellar/launcher"
 )
 
 func main() {
-	i := NewInterstellar()
-	if err := launcher.Run(i); err != nil {
-		log.Println(err)
-	}
+	launcher.Run(NewInterstellar())
 }
--- launcher/launcher.go
+++ launcher/launcher.go
@@ -1,5 +1,15 @@
 package launcher
 
-func Run(l Launcher) error {
-	return nil
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Run(l Launcher) {
+	g := gin.New()
+
+	if err := g.Run(l.Config().Port); err != nil {
+		log.Fatalf("run broker: %v", err)
+	}
 }
